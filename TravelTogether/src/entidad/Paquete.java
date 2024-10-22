ac5622542a3492f48ed73e8d5ce9af142d613ef0
Lautaro-10 <laucadelago123@gmail.com>
2024-10-22 19:17:58 -0300

    Alojamiento

--- TravelTogether/src/entidad/Paquete.java
+++ TravelTogether/src/entidad/Paquete.java
@@ -9,6 +9,102 @@ package entidad;
  *
  * @author Celi Leandro
  */
+
 public class Paquete {
-    
+
+    private int codPaquete;
+    private String fechaIni;
+    private String fechaFin;
+    private double boleto;
+    private int estadia;
+    private int regimen;
+    private int destino;
+    private float traslados;
+    private double montoFinal;
+
+    public Paquete() {}
+
+    public Paquete(int codPaquete, String fechaIni, String fechaFin, double boleto, int estadia, int regimen, int destino, float traslados, double montoFinal) {
+        this.codPaquete = codPaquete;
+        this.fechaIni = fechaIni;
+        this.fechaFin = fechaFin;
+        this.boleto = boleto;
+        this.estadia = estadia;
+        this.regimen = regimen;
+        this.destino = destino;
+        this.traslados = traslados;
+        this.montoFinal = montoFinal;
+    }
+
+    public int getCodPaquete() {
+        return codPaquete;
+    }
+
+    public void setCodPaquete(int codPaquete) {
+        this.codPaquete = codPaquete;
+    }
+
+    public String getFechaIni() {
+        return fechaIni;
+    }
+
+    public void setFechaIni(String fechaIni) {
+        this.fechaIni = fechaIni;
+    }
+
+    public String getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(String fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public double getBoleto() {
+        return boleto;
+    }
+
+    public void setBoleto(double boleto) {
+        this.boleto = boleto;
+    }
+
+    public int getEstadia() {
+        return estadia;
+    }
+
+    public void setEstadia(int estadia) {
+        this.estadia = estadia;
+    }
+
+    public int getRegimen() {
+        return regimen;
+    }
+
+    public void setRegimen(int regimen) {
+        this.regimen = regimen;
+    }
+
+    public int getDestino() {
+        return destino;
+    }
+
+    public void setDestino(int destino) {
+        this.destino = destino;
+    }
+
+    public float getTraslados() {
+        return traslados;
+    }
+
+    public void setTraslados(float traslados) {
+        this.traslados = traslados;
+    }
+
+    public double getMontoFinal() {
+        return montoFinal;
+    }
+
+    public void setMontoFinal(double montoFinal) {
+        this.montoFinal = montoFinal;
+    }
 }
--- TravelTogether/src/entidad/Pasaje.java
+++ TravelTogether/src/entidad/Pasaje.java
@@ -10,5 +10,72 @@ package entidad;
  * @author Celi Leandro
  */
 public class Pasaje {
-    
+
+    private int codPasaje;
+    private String fechaHora;
+    private String origen;
+    private String destino;
+    private int asiento;
+    private String transporte;
+
+    public Pasaje() {}
+
+    public Pasaje(int codPasaje, String fechaHora, String origen, String destino, int asiento, String transporte) {
+        this.codPasaje = codPasaje;
+        this.fechaHora = fechaHora;
+        this.origen = origen;
+        this.destino = destino;
+        this.asiento = asiento;
+        this.transporte = transporte;
+    }
+
+    // Getters y Setters
+    public int getCodPasaje() {
+        return codPasaje;
+    }
+
+    public void setCodPasaje(int codPasaje) {
+        this.codPasaje = codPasaje;
+    }
+
+    public String getFechaHora() {
+        return fechaHora;
+    }
+
+    public void setFechaHora(String fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getAsiento() {
+        return asiento;
+    }
+
+    public void setAsiento(int asiento) {
+        this.asiento = asiento;
+    }
+
+    public String getTransporte() {
+        return transporte;
+    }
+
+    public void setTransporte(String transporte) {
+        this.transporte = transporte;
+    }
 }
+
