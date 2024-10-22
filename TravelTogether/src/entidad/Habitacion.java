d7c41cf2a73515e018d22d4c4cf86791771e3ee6
MilianCaceres <miliancaceres0@gmail.com>
2024-10-22 18:10:57 -0300

    Habitacion

--- TravelTogether/src/entidad/Habitacion.java
+++ TravelTogether/src/entidad/Habitacion.java
@@ -1,14 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidad;
 
-/**
- *
- * @author Celi Leandro
- */
+
 public class Habitacion {
+        
+    private int nroHabitacion;
+    private int planta;
+    private int numeracion;
+    private int cupo;
+    private boolean estado;
+
+    public Habitacion(int nroHabitacion, int planta, int numeracion, int cupo, boolean estado) {
+        this.nroHabitacion = nroHabitacion;
+        this.planta = planta;
+        this.numeracion = numeracion;
+        this.cupo = cupo;
+        this.estado = estado;
+    }
+
+    public int getNroHabitacion() {
+        return nroHabitacion;
+    }
+
+    public void setNroHabitacion(int nroHabitacion) {
+        this.nroHabitacion = nroHabitacion;
+    }
+
+    public int getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(int planta) {
+        this.planta = planta;
+    }
+
+    public int getNumeracion() {
+        return numeracion;
+    }
+
+    public void setNumeracion(int numeracion) {
+        this.numeracion = numeracion;
+    }
+
+    public int getCupo() {
+        return cupo;
+    }
+
+    public void setCupo(int cupo) {
+        this.cupo = cupo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero de Habitacion: "+nroHabitacion+ 
+                ", Planta: "+planta+ 
+                ", Numeracion: "+numeracion+ 
+                ", Cupo: "+cupo+ 
+                ", Estado: " +estado;
+    }
+    
     
 }
