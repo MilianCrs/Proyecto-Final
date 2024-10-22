9b288dce7f965857fa9a64e9b2bef8aa0efbd80e
Lautaro-10 <laucadelago123@gmail.com>
2024-10-22 19:17:52 -0300

    Alojamiento

--- TravelTogether/src/entidad/Alojamiento.java
+++ TravelTogether/src/entidad/Alojamiento.java
@@ -10,5 +10,92 @@ package entidad;
  * @author Celi Leandro
  */
 public class Alojamiento {
+
+    private int codAlojam;
+    private String nombre;
+    private int capacidad;
+    private int nroAmbientes;
+    private String camas;
+    private int banios;
+    private double precioNoche;
+    private String tipo;
+
     
+    public Alojamiento() {}
+
+    public Alojamiento(int codAlojam, String nombre, int capacidad, int nroAmbientes, String camas, int banios, double precioNoche, String tipo) {
+        this.codAlojam = codAlojam;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.nroAmbientes = nroAmbientes;
+        this.camas = camas;
+        this.banios = banios;
+        this.precioNoche = precioNoche;
+        this.tipo = tipo;
+    }
+
+    
+    public int getCodAlojam() {
+        return codAlojam;
+    }
+
+    public void setCodAlojam(int codAlojam) {
+        this.codAlojam = codAlojam;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public int getNroAmbientes() {
+        return nroAmbientes;
+    }
+
+    public void setNroAmbientes(int nroAmbientes) {
+        this.nroAmbientes = nroAmbientes;
+    }
+
+    public String getCamas() {
+        return camas;
+    }
+
+    public void setCamas(String camas) {
+        this.camas = camas;
+    }
+
+    public int getBanios() {
+        return banios;
+    }
+
+    public void setBanios(int banios) {
+        this.banios = banios;
+    }
+
+    public double getPrecioNoche() {
+        return precioNoche;
+    }
+
+    public void setPrecioNoche(double precioNoche) {
+        this.precioNoche = precioNoche;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
